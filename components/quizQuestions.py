ca0acc0360cd925ea12656fd6271fab414f09253
Alexandra Lem <a_lem@fanshaweonline.ca>
2021-12-10 09:01:18 +0600

    adding game main logic files

---
+++ components/quizQuestions.py
@@ -0,0 +1,38 @@
+questions = {
+    "q1" : {
+        "question":"Were you born on Earth? ",
+        "yes" : 0,
+        "no" : 500
+    },
+
+    "q2" : {
+        "question":"Do you have superpowers? ",
+        "yes" : 0,
+        "no" : 100
+    },
+
+     "q3" : {
+        "question":"Are you female? ",
+        "yes" : 1000,
+        "no" : 0
+    },
+
+     "q4" : {
+        "question":"Can you fly? ",
+        "yes" : 50,
+        "no" : 0
+    },
+
+     "q5" : {
+        "question":"Do you carry a weapon? ",
+        "yes" : 10,
+        "no" : 0
+    },
+
+    #Born on Earth: Hulk, Captain America, Hawkeye, Captain Marvel
+    #Have superpowers: Hulk, Captain America, Thor, Captain Marvel
+    #Female: Captain Marvel
+    #Can fly: Thor, Captain Marvel
+    #Carrries weapon: Captain America, Hawkeye, Thor
+
+}
